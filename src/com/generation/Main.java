a763983fc05ad5616b132e0c8922c638093e3a7b
Diana Marlene <diana.sanchez9907@gmail.com>
2021-10-21 10:13:28 -0500

    Exceptions

--- src/com/generation/Main.java
+++ src/com/generation/Main.java
@@ -4,17 +4,30 @@ import com.generation.paquete1.Person;
 
 public class Main {
 
+    public static int numerador = 10;
+    public static int denominador =0;
+    public static float  division;
+
     public static void main(String[] args) {
         /* write your code here */
 
+        System.out.println("Antes de hacer la división");
+        try{
+            division = numerador /denominador;
+        }catch (NullPointerException ex) {
+            System.out.println("Null pointer");
+            System.out.println(ex.getMessage());
+        }
 
-        Person person= new Person();
+        System.out.println("Después de la división");
+
+        /*Person person= new Person();
         person.setName("Santiago");
         person.setAge(33);
         person.setId("102045455");
         System.out.println( "Name: " + person.getName());
         System.out.println( "Age: "+ person.getAge());
-        System.out.println( "Id: " +  person.getId());
+        System.out.println( "Id: " +  person.getId());*/
 
         /*CuentaBancaria cuenta = new CuentaBancaria();
         cuenta.setNombre("Diana Sánchez");
